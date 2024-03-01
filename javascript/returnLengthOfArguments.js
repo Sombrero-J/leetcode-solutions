25661d11edfd803335ef87dd8461766e00bacf62
Weng Chan <wengchan99899@gmail.com>
2024-03-01 18:47:20 +0000

    Create returnLengthOfArguments.js

---
+++ javascript/returnLengthOfArguments.js
@@ -0,0 +1,17 @@
+// Write a function argumentsLength that returns the count of arguments passed to it.
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    let i = 0;
+    args.forEach(() => {
+        i++;
+    });
+    return i;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
